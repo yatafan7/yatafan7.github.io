00899e3e82f44a846fa9c102b69809d7400d5098
yatafan7 <yatauwu@gmail.com>
2025-10-29 18:09:13 -0400

    okay it shouldnt fuck with android chrome/

--- ihatelinux.js
+++ ihatelinux.js
@@ -1,8 +1,7 @@
 var audio = new Audio('https://yata.win/No%20diddy.mp3');
-    audio.play()
 let os=navigator.userAgent;
 if(os.includes("Linux") && (!os.includes("Android"))){
     alert("stop using linux chud");
-    "play()";
+    audio.play()
 
 }
