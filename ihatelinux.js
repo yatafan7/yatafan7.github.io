a84f6bee023411aae2959c603d1bcea189a339c2
yatafan7 <yatauwu@gmail.com>
2025-10-29 05:51:06 -0400

    hope it works. that was really annoying.

--- ihatelinux.js
+++ ihatelinux.js
@@ -1,7 +1,8 @@
 var audio = new Audio('https://yata.win/No%20diddy.mp3');
-    audio.play();
+    audio.play()
 let os=navigator.userAgent;
 if(os.includes("Linux") && (!os.includes("Android"))){
-    alert("stop using linux chud")
-    "play()"
+    alert("stop using linux chud");
+    "play()";
+
 }
